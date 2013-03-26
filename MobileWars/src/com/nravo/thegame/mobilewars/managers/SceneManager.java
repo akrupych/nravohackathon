f76aa508b0bdf851b102310ce530d1663f6cb665
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-26 23:10:33 +0000

    Developing SceneMnager.

--- MobileWars/src/com/nravo/thegame/mobilewars/managers/SceneManager.java
+++ MobileWars/src/com/nravo/thegame/mobilewars/managers/SceneManager.java
@@ -1,6 +1,55 @@
 package com.nravo.thegame.mobilewars.managers;
 
+import com.nravo.thegame.mobilewars.runtime.ManagedScene;
+import org.andengine.engine.Engine;
+import org.andengine.engine.handler.IUpdateHandler;
+
 public class SceneManager {
     private static final SceneManager INSTANCE = new SceneManager();
+
+    private SceneManager() {
+    }
+
+    public static SceneManager getInstance() {
+        return INSTANCE;
+    }
+
+    public ManagedScene currentScene;
+    private ManagedScene mNextScene;
+    private Engine mEngine = ResourceManager.getInstance().engine;
+
+    private int mNumberOfFramesPassed = -1;
+    private boolean mLoadingScreenHandlerRegistered = false;
+
+    private IUpdateHandler mLoadingScreenHandler = new IUpdateHandler() {
+        @Override
+        public void onUpdate(float pSecondsElapsed) {
+            mNumberOfFramesPassed++;
+            mNextScene.elapsedLoadingScreenTime += pSecondsElapsed;
+            if (mNumberOfFramesPassed == 1) {
+                if (currentScene != null) {
+                    currentScene.onHideManagedScene();
+                    currentScene.onUnloadManagedScene();
+                }
+                mNextScene.onLoadManagedScene();
+            }
+
+            if (mNumberOfFramesPassed > 1 && mNextScene.elapsedLoadingScreenTime >= mNextScene.minLoadingScreenTime) {
+                mNextScene.clearChildScene();;
+                mNextScene.onLoadingScreenUnloadAndHidden();
+                mNextScene.onShowManagedScene();
+                currentScene = mNextScene;
+
+                // reset the handler and loading screen variables for another use
+                mNextScene.elapsedLoadingScreenTime = 0;
+                mNumberOfFramesPassed = -1;
+                mEngine.unregisterUpdateHandler(this);
+                mLoadingScreenHandlerRegistered = false;
+            }
+        }
+
+        @Override public void reset() {}
+    };
+
 
 }
