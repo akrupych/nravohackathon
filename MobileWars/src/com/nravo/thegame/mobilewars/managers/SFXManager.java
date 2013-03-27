849a3738da9f3dd609edaa5f46c0a9bf0c75496f
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-27 23:33:40 +0000

    Music playback added.

--- MobileWars/src/com/nravo/thegame/mobilewars/managers/SFXManager.java
+++ MobileWars/src/com/nravo/thegame/mobilewars/managers/SFXManager.java
@@ -1,7 +1,40 @@
 package com.nravo.thegame.mobilewars.managers;
 
+import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.audio.sound.Sound;
+import org.andengine.util.debug.Debug;
+
+import java.io.IOException;
+
 /**
  * Handles the playback of music and sounds as well as their muted state
  */
 public class SFXManager {
+    private static final SFXManager INSTANCE = new SFXManager();
+
+    private static Music mMusic;
+
+    private static Sound mExplosion;
+
+    public static SFXManager getInstance() {
+        return INSTANCE;
+    }
+
+    public boolean mSoundsMuted;
+    public boolean mMusicMuted;
+
+    private SFXManager() {
+        MusicFactory.setAssetBasePath("sounds/");
+        try {
+            mMusic = MusicFactory.createMusicFromAsset(ResourceManager.getActivity().getMusicManager(),
+                    ResourceManager.getActivity(), "music.ogg");
+        } catch (IOException e) {
+            Debug.e(e);
+        }
+    }
+
+    public static void playMusic() {
+        mMusic.play();
+    }
 }
