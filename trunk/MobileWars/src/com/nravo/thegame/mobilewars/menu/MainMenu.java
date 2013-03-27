d436051df943c2f1c2cc005aaad91768cbaf6593
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-27 13:58:58 +0000

    MainMenu background is shown on startup.

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/menu/MainMenu.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/menu/MainMenu.java
@@ -1,6 +1,9 @@
 package com.nravo.thegame.mobilewars.menu;
 
+import com.nravo.thegame.mobilewars.managers.ResourceManager;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
 
 public class MainMenu extends ManagedMenuScene {
 
@@ -18,31 +21,46 @@ public class MainMenu extends ManagedMenuScene {
 
     @Override
     public Scene onLoadingScreenLoadAndShown() {
-        return null;  // No loading screen
+        throw new UnsupportedOperationException();  // No loading screen for menu - no need to implement this
     }
 
     @Override
     public void onLoadingScreenUnloadAndHidden() {
-        // No loading screen
+        throw new UnsupportedOperationException(); // No loading screen for menu - no need to implement this
     }
+
+    private Sprite mBackgroundSprite;
 
     @Override
     public void onLoadScene() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        ResourceManager.loadMenuResources();
+
+        mBackgroundSprite = new Sprite(ResourceManager.getInstance().cameraWidth/2f,
+                ResourceManager.getInstance().cameraHeight/2f,
+                ResourceManager.sMenuBackgroundTR, ResourceManager.getInstance().engine.getVertexBufferObjectManager());
+        mBackgroundSprite.setScale(ResourceManager.getInstance().cameraHeight / ResourceManager.sMenuBackgroundTR.getHeight());
+        mBackgroundSprite.setZIndex(-5000);
+        this.attachChild(mBackgroundSprite);
+
+        Rectangle rectangle = new Rectangle(0, 0, 200, 200, ResourceManager.getInstance().engine.getVertexBufferObjectManager());
+        rectangle.setColor(0, 0, 1);
+        this.attachChild(rectangle);
+
+        // create the background
     }
 
     @Override
     public void onShowScene() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        Rectangle rectangle = new Rectangle(200, 200, 200, 200, ResourceManager.getInstance().engine.getVertexBufferObjectManager());
+        rectangle.setColor(0, 0, 1);
+        this.attachChild(rectangle);
     }
 
     @Override
     public void onHideScene() {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public void onUnloadScene() {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
