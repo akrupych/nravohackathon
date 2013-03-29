1afd0e0e78022f0e67039cc9441cda7105a549c1
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-29 16:08:38 +0000

    Added stubs for Game layers. (layer package)

---
+++ MobileWars/src/com/nravo/thegame/mobilewars/layers/OptionsLayer.java
@@ -0,0 +1,27 @@
+package com.nravo.thegame.mobilewars.layers;
+
+/**
+ * Accessible from the main menu and allows the player to enable or disable
+ * music and sounds etc.
+ */
+public class OptionsLayer extends ManagedLayer {
+    @Override
+    public void onLoadLayer() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onShowLayer() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onHideLayer() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onUnloadLayer() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
