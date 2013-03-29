1e431113a29d6f7953429715eccc72a35674d4bd
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-29 22:10:07 +0000

    Level class added to store levels.

---
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/Levels.java
@@ -0,0 +1,55 @@
+package com.nravo.thegame.mobilewars.gamelevel;
+
+/**
+ * Contains an array of levels that can be played in the game as well as helper methods
+ * to retrieve specific levels
+ */
+public class Levels {
+
+    private static final LevelDefinition[] AVAILABLE_LEVELS = new LevelDefinition[] {
+            new LevelDefinition(1,
+            new BuildingDefinition[] {
+                new BuildingDefinition(0, 0, Race.ANDROID, BuildingDefinition.BuildingType.SIMPLE),
+                new BuildingDefinition(0, 0, Race.APPLE_IOS, BuildingDefinition.BuildingType.TOWER)
+            }),
+    };
+
+    public static class BuildingDefinition {
+
+        public enum BuildingType {
+            SIMPLE,
+            BATTERY,
+            TOWER
+        }
+
+        public final float x;
+        public final float y;
+        public final Race race; // operating system it belongs to in the beginning
+        public final BuildingType buildingType;
+
+        public BuildingDefinition(float x, float y, Race race, BuildingType buildingType) {
+            this.x = x;
+            this.y = y;
+            this.race = race;
+            this.buildingType = buildingType;
+        }
+
+    }
+
+    /**
+     * An object that contains all the information to construct a level
+     */
+    public static class LevelDefinition {
+        public final int levelIndex;
+        public final  BuildingDefinition[] buildingsInLevel;
+
+        public LevelDefinition(int levelIndex, BuildingDefinition[] buildings) {
+            this.levelIndex = levelIndex;
+            this.buildingsInLevel = buildings;
+        }
+    }
+
+    public enum Race {
+        ANDROID, APPLE_IOS
+    }
+}
