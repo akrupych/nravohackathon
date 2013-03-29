ead4d19a12c611758cef69cc7277540baefb65e3
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-29 21:22:08 +0000

    Music toggle added.

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/menu/MainMenu.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/menu/MainMenu.java
@@ -2,7 +2,9 @@ package com.nravo.thegame.mobilewars.menu;
 
 import com.nravo.thegame.mobilewars.gamelevel.GameLevel;
 import com.nravo.thegame.mobilewars.input.GrowButton;
+import com.nravo.thegame.mobilewars.input.GrowToggleButton;
 import com.nravo.thegame.mobilewars.managers.ResourceManager;
+import com.nravo.thegame.mobilewars.managers.SFXManager;
 import com.nravo.thegame.mobilewars.managers.SceneManager;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
@@ -50,6 +52,19 @@ public class MainMenu extends ManagedMenuScene {
         mMenuBackgroundSprite.setZIndex(-5000);
         this.attachChild(mMenuBackgroundSprite);
 
+        GrowToggleButton musicToggleButton = new GrowToggleButton(ResourceManager.musicToggleTTR.getWidth() / 2,
+                ResourceManager.musicToggleTTR.getHeight() / 2, ResourceManager.musicToggleTTR, !SFXManager.isMusicMuted()) {
+            @Override
+            public void onClick() {
+                SFXManager.toggleMusicMuted();
+            }
+
+            @Override
+            public boolean checkState() {
+                return !SFXManager.isMusicMuted();
+            }
+        };
+
         Rectangle rectangle = new Rectangle(0, 0, 200, 200, ResourceManager.getInstance().engine.getVertexBufferObjectManager());
         rectangle.setColor(0, 0, 1);
         this.attachChild(rectangle);
@@ -60,8 +75,11 @@ public class MainMenu extends ManagedMenuScene {
                 SceneManager.getInstance().showScene(new GameLevel());
             }
         };
+
+        this.attachChild(musicToggleButton);
         this.attachChild(playButton);
         this.registerTouchArea(playButton);
+        this.registerTouchArea(musicToggleButton);
 
     }
 
