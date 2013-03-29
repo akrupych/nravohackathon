78fe090aa6b956d260f07c5c1f39d925bc894aef
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-03-29 12:27:25 +0000

    GameManager implemented.

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/managers/GameManager.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/managers/GameManager.java
@@ -1,7 +1,51 @@
 package com.nravo.thegame.mobilewars.managers;
 
+import com.nravo.thegame.mobilewars.gamelevel.GameLevel;
+import org.andengine.engine.handler.IUpdateHandler;
+
 /**
  * Monitors the game and checks whether the player won or lost
  */
-public class GameManager {
+public class GameManager implements IUpdateHandler {
+
+    public interface GameLevelGoal {
+        public boolean isLevelCompleted();
+        public void onLevelCompleted();
+        public boolean isLevelFailed();
+        public void onLevelFailed();
+    }
+
+    private static final GameManager INSTANCE = new GameManager();
+
+    private GameLevelGoal mGameLevelGoal;
+    private GameLevel mGameLevel;
+
+    @Override
+    public void onUpdate(float pSecondsElapsed) {
+        if (mGameLevelGoal != null) {
+            if (mGameLevelGoal.isLevelCompleted()) {
+                mGameLevelGoal.onLevelCompleted();
+                mGameLevelGoal = null;
+            } else if (mGameLevelGoal.isLevelFailed()) {
+                mGameLevelGoal.onLevelFailed();
+                mGameLevelGoal = null;
+            }
+        }
+    }
+
+    public static void setGameLevel(GameLevel gameLevel) {
+        INSTANCE.mGameLevel = gameLevel;
+    }
+
+    public static GameLevel getGameLevel() {
+        return INSTANCE.mGameLevel;
+    }
+
+    public static void setGameLevelGoal(GameLevelGoal gameLevelGoal) {
+        INSTANCE.mGameLevelGoal = gameLevelGoal;
+    }
+
+    @Override
+    public void reset() { }
+
 }
