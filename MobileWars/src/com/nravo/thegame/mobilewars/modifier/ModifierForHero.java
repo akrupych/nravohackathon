4f725987593047a6f087570896335ffb907037b1
nadiialendyel@gmail.com <nadiialendyel@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 12:44:59 +0000

    Modifier added.

---
+++ MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
@@ -0,0 +1,18 @@
+package com.nravo.thegame.mobilewars.modifier;
+
+import org.andengine.entity.modifier.SequenceEntityModifier;
+
+import com.nravo.thegame.mobilewars.entity.Hero;
+
+public class ModifierForHero extends SequenceEntityModifier {
+
+	public Hero hero;
+	
+	public ModifierForHero(Hero hero) {
+		
+		//this.addModifierListener(pModifierListener);
+	}
+
+
+	
+}
