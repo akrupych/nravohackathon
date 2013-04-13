ff9b0b90bbc7ce8657dc89c289ebc652ac35482b
nadiialendyel@gmail.com <nadiialendyel@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 15:18:29 +0000

    implement speed for hero

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
@@ -1,15 +1,31 @@
 package com.nravo.thegame.mobilewars.modifier;
 
-import com.nravo.thegame.mobilewars.entity.Building;
-import org.andengine.entity.modifier.MoveModifier;
-
 import java.util.List;
+
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.util.modifier.IModifier;
+
+import com.nravo.thegame.mobilewars.entity.Building;
 
 public class ModifierForHero extends MoveModifier {
 	
-	public ModifierForHero(float duration, float fromX, float fromY, float toX, float toY,
-                           final List<Building> from, final Building to) {
-		super (5, fromX, fromY, toX, toY);
+	public ModifierForHero(float duration, float fromX, float fromY, float toX, float toY, final List<Building> from, final Building to) {
+		super (duration, fromX,fromY,toX,toY);	
+		this.addModifierListener(new IModifierListener<IEntity>() {			
+			@Override
+			public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
+				for (Building building: from){
+					building.decrementNumberOfUnits(1);
+				}	
+				from.clear();
+			}
+			
+			@Override
+			public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
+				to.incrementNumberOfUnits(1);					
+			}
+		});
 	}
 	
 }
