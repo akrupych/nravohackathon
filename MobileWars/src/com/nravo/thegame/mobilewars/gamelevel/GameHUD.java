9f2f4cd9800eb365b541fab69aba2c9cb8f4b8a3
andriy.krupych@gmail.com <andriy.krupych@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 21:16:45 +0000

    Added Jelly Beans effect

--- MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/GameHUD.java
+++ MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/GameHUD.java
@@ -1,18 +1,52 @@
 package com.nravo.thegame.mobilewars.gamelevel;
 
 import org.andengine.engine.camera.hud.HUD;
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 
+import com.nravo.thegame.mobilewars.effects.JellyBeansEffect.State;
 import com.nravo.thegame.mobilewars.managers.ResourceManager;
 
 public class GameHUD extends HUD {
 	
-	public GameHUD() {
+	public GameHUD(Scene scene) {
+		
         setScaleCenter(0f, 0f);
         setScale(ResourceManager.getInstance().cameraScaleFactorX,
                 ResourceManager.getInstance().cameraScaleFactorY);
-        attachChild(new Sprite(0, 0, ResourceManager.sHudBackgroundTR,
-        		ResourceManager.getEngine().getVertexBufferObjectManager()));
+        
+        Sprite backround = new Sprite(0, 0, ResourceManager.sHudBackgroundTR,
+        		ResourceManager.getEngine().getVertexBufferObjectManager());
+        backround.setAnchorCenter(0, 0);
+        attachChild(backround);
+        
+        Sprite honeycomb = new Sprite(0, 0, ResourceManager.sPowerHoneycombTR,
+        		ResourceManager.getEngine().getVertexBufferObjectManager());
+        honeycomb.setAnchorCenter(0, 0);
+        attachChild(honeycomb);
+        
+        Sprite iceCreamSandwich = new Sprite(128, 0,
+        		ResourceManager.sPowerIceCreamSandwichTR,
+        		ResourceManager.getEngine().getVertexBufferObjectManager());
+        iceCreamSandwich.setAnchorCenter(0, 0);
+        attachChild(iceCreamSandwich);
+        
+        Sprite jellyBeans = new Sprite(256, 0,
+        		ResourceManager.sPowerJellyBeansTR,
+        		ResourceManager.getEngine().getVertexBufferObjectManager()) {
+        	public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+        			float pTouchAreaLocalX, float pTouchAreaLocalY) {
+        		setScale(2);
+        		GameLevel.mJellyBeansEffect.mState = State.WAITING;
+        		return true;
+        	};
+        };
+        jellyBeans.setScaleCenter(jellyBeans.getX() + jellyBeans.getWidth() / 2,
+        		jellyBeans.getY() + jellyBeans.getHeight() / 2);
+        jellyBeans.setAnchorCenter(0, 0);
+        scene.registerTouchArea(jellyBeans);
+        attachChild(jellyBeans);
 	}
 	
 }
