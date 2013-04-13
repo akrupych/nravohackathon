4eb30ddaed5f7242f2ab9dd862b2e8d6a796b1ef
nadiialendyel@gmail.com <nadiialendyel@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 11:12:59 +0000

    change to public hero animated sprite

--- MobileWars/src/com/nravo/thegame/mobilewars/entity/HeroAndroid.java
+++ MobileWars/src/com/nravo/thegame/mobilewars/entity/HeroAndroid.java
@@ -4,13 +4,15 @@ import org.andengine.entity.sprite.AnimatedSprite;
 
 import com.nravo.thegame.mobilewars.managers.ResourceManager;
 
-public class HeroAndroid extends Hero {
+public class HeroAndroid extends Hero{
+
 
 	public HeroAndroid() {
-		hero = new AnimatedSprite(fromX, fromY, ResourceManager.sAndroidTTR,
-				ResourceManager.getActivity().getVertexBufferObjectManager());
-
-		hero.animate(new long[] { 150, 150, 150, 150 });
+			hero = new AnimatedSprite(fromX, fromY, ResourceManager.sAndroidTTR,
+					ResourceManager.getActivity()
+							.getVertexBufferObjectManager());
+		
+			hero.animate(new long[] { 150, 150, 150, 150 });	
+		
 	}
-
 }
