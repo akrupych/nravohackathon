2be482f6df92001d3222e21abea28012eb0694aa
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 09:24:32 +0000

    UnitMovementHandler added.

---
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/handlers/UnitMovementHandler.java
@@ -0,0 +1,22 @@
+package com.nravo.thegame.mobilewars.gamelevel.handlers;
+
+import org.andengine.engine.handler.IUpdateHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Taras Osiris
+ * Date: 4/13/13
+ * Time: 12:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnitMovementHandler implements IUpdateHandler {
+    @Override
+    public void onUpdate(float pSecondsElapsed) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void reset() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
