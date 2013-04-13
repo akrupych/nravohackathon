efe02608c3a9750a427d2e74e2dec12edc846a08
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 16:37:28 +0000

    Sprite pool updated.

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
@@ -1,8 +1,10 @@
 package com.nravo.thegame.mobilewars.modifier;
 
+import com.nravo.thegame.mobilewars.entity.AndroidSpritePool;
 import com.nravo.thegame.mobilewars.entity.Building;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.util.modifier.IModifier;
 
 import java.util.List;
@@ -10,19 +12,21 @@ import java.util.List;
 public class ModifierForHero extends MoveModifier {
 	
 	public ModifierForHero(float duration, float fromX, float fromY, float toX, float toY,
-                           final List<Building> from, final Building to) {
-		super (duration, fromX,fromY,toX,toY);	
+                           final List<Building> buildingsFrom, final Building buildingTo,
+                           final AndroidSpritePool pool, AnimatedSprite sprite) {
+		super(duration,fromX, fromY, toX, toY);
 		this.addModifierListener(new IModifierListener<IEntity>() {
             @Override
             public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
-                for (Building building : from) {
+                for (Building building : buildingsFrom) {
                     building.decrementNumberOfUnits(1);
                 }
+                buildingsFrom.clear();
             }
 
             @Override
             public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
-                to.incrementNumberOfUnits(1);
+                buildingTo.incrementNumberOfUnits(1);
             }
         });
 	}
