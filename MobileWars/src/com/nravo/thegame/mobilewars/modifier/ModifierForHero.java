7c0e5a4c1918ec5127554f78151075e5a07a489f
leskiv.taras@gmail.com <leskiv.taras@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-13 16:40:02 +0000

    Sprite pool updated.

--- MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
+++ MobileWars/src/com/nravo/thegame/mobilewars/modifier/ModifierForHero.java
@@ -2,9 +2,9 @@ package com.nravo.thegame.mobilewars.modifier;
 
 import com.nravo.thegame.mobilewars.entity.AndroidSpritePool;
 import com.nravo.thegame.mobilewars.entity.Building;
+import com.nravo.thegame.mobilewars.entity.Hero;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.MoveModifier;
-import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.util.modifier.IModifier;
 
 import java.util.List;
@@ -13,7 +13,7 @@ public class ModifierForHero extends MoveModifier {
 	
 	public ModifierForHero(float duration, float fromX, float fromY, float toX, float toY,
                            final List<Building> buildingsFrom, final Building buildingTo,
-                           final AndroidSpritePool pool, AnimatedSprite sprite) {
+                           final AndroidSpritePool pool, Hero hero) {
 		super(duration,fromX, fromY, toX, toY);
 		this.addModifierListener(new IModifierListener<IEntity>() {
             @Override
