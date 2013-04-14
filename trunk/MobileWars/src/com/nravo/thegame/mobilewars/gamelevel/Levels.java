2b54fae9fbf9997d1866b1951b80ecc8cdcbd83a
nadiialendyel@gmail.com <nadiialendyel@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-14 12:34:26 +0000

    add new levels

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/Levels.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/gamelevel/Levels.java
@@ -29,6 +29,19 @@ public class Levels {
                 new BuildingDefinition(0.80f*CAMERA_WIDTH, 0.25f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
                 new BuildingDefinition(0.94f*CAMERA_WIDTH, 0.44f*CAMERA_HEIGHT, 15, Race.APPLE_IOS, BuildingDefinition.BuildingType.TOWER),
             }),
+            new LevelDefinition(2,
+                    new BuildingDefinition[] {
+                        new BuildingDefinition(0.09f*CAMERA_WIDTH, 0.8f*CAMERA_HEIGHT, 10, Race.APPLE_IOS, BuildingDefinition.BuildingType.SIMPLE),
+                        new BuildingDefinition(0.87f*CAMERA_WIDTH, 0.18f*CAMERA_HEIGHT, 10, Race.ANDROID, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.14f*CAMERA_WIDTH, 0.15f*CAMERA_HEIGHT, 10, Race.APPLE_IOS, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.07f*CAMERA_WIDTH, 0.5f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.3f*CAMERA_WIDTH, 0.65f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.85f*CAMERA_WIDTH, 0.78f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.65f*CAMERA_WIDTH, 0.42f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.6f*CAMERA_WIDTH, 0.75f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.47f*CAMERA_WIDTH, 0.21f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                        new BuildingDefinition(0.93f*CAMERA_WIDTH, 0.5f*CAMERA_HEIGHT, 10, Race.NEUTRAL, BuildingDefinition.BuildingType.TOWER),
+                  })
     };
 
     public static class BuildingDefinition {
