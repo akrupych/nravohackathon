0fd0f8ff2cf14b7072d6ac20cfc818f69e52f5ea
nadiialendyel@gmail.com <nadiialendyel@gmail.com@1a8a984c-fb13-62ec-b103-0fa2c24e7cbb>
2013-04-14 06:13:36 +0000

    change background for powers

--- trunk/MobileWars/src/com/nravo/thegame/mobilewars/entity/HeroApple.java
+++ trunk/MobileWars/src/com/nravo/thegame/mobilewars/entity/HeroApple.java
@@ -1,5 +1,9 @@
 package com.nravo.thegame.mobilewars.entity;
 
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.RotationModifier;
+import org.andengine.entity.sprite.Sprite;
+
 public class HeroApple extends Hero {
 
 	public HeroApple(float fromX, float fromY, float toX, float toY,
@@ -9,4 +13,14 @@ public class HeroApple extends Hero {
 		heroSprite.animate(new long[] { 250, 250, 250, 250, 250 });
 	}
 
+	/*public void moveHero(float fromX, float fromY, float toX, float toY) {
+		if (fromX< toX){
+		
+		}
+		RotationModifier rotate = new RotationModifier(2, 0, 360);		
+		LoopEntityModifier loop = new LoopEntityModifier(rotate);
+		heroSprite.setRotationCenter(0,0);
+		heroSprite.registerEntityModifier(loop);
+	}*/
+	
 }
